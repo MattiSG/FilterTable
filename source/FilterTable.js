3d3409b51f135ad23689842272d0e53690e863f0
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2011-08-30 10:59:27 +0200

    Big performance improvement. RegExp search.

--- source/FilterTable.js
+++ source/FilterTable.js
@@ -28,7 +28,22 @@ var FilterTable = new Class({
     	*Set to false if all tables, whatever their classes, should be matched.
     	*/
          filterClass: 'filterable',
-         placeholder: 'Filter…'
+         
+         /**Input placeholder.
+         */
+         placeholder: 'Filter…',
+         
+         /**Regexp options.
+         */
+         match: {
+         	/**Allow regexp input or not?
+         	*/
+         	regexp: true,
+	         /**RegExp match flags.
+	         *Defaults to 'i' (case-insensitive)
+	         */
+         	flags: 'i'
+         }
     },
 
     /* 
@@ -68,22 +83,26 @@ var FilterTable = new Class({
      * @param table (Element)
      * @public
      */
-    filterTable: function(term, table) {
-         //split by space the search terms
-         var terms = term.value.toLowerCase().split(' ');
+    filterTable: function(input, table) {
+    	var regexp = input.get('value')
+    					  .trim() // avoid that a space at the end matches all
+    					  .split(' ')
+    					  .join('|'); // each word is a different match
+    					  
+    	if (! this.options.match.regexp)
+    		regexp = regexp.escapeRegExp();
+    	
+    	//create a compiled regexp from search terms for performance
+    	regexp = new RegExp(regexp, this.options.match.flags);
+    	
          //for each row of table execute
-         for (var r = 0, m = table.rows.length; r < m; r++) {
-             var display = '';
-             //for each term do
-             for (var i = 0, j = terms.length; i < j; i++) {
-                 //strips all tags from row, then test if the 
-                 //row contains or not the appropriate filter term.
-                 if (table.rows[r].innerHTML.replace(/<[^>]+>/g, "").toLowerCase().indexOf(terms[i]) < 0) {
-                    display = 'none';
-                }
-                 //display the row
-                table.rows[r].style.display = display;
-             }
-        }
+         table.getElements('tbody tr').each(function(tr) {
+             tr.setStyle('display',
+             	(tr.get('text').match(regexp)
+             		? ''
+             		: 'none'
+             	)
+             );
+         });
     }
 });
